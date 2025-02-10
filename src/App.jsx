7b43f839451d1af3bf6cc822e4675688f8ad6334
HeedlessGentry <chrisgithinji286@gmail.com>
2025-02-10 10:34:16 +0300

    Test 103

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from "./LandingPage";
 import ExpenseTracker from './ExpenseTracker';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <Router>
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-          <LandingPage />
-          </Route>
-          <Route path="/tracker">
-          <ExpenseTracker />
-          </Route>
-        </Switch>
+      <div className="app-container">
+        <Navbar />
+        <div className="app">
+          <Routes>
+            <Route exact path='/' element={<LandingPage />} />
+            <Route path='/tracker' element={<ExpenseTracker />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css';
+
+function Navbar () {
+    return (
+        <nav className="navbar">
+            <h1 className="site-name">
+                <Link to = "/">Cashflow Records</Link>
+            </h1>
+            <div className="nav-links">
+                <Link to="/tracker">Expense Tracker</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
